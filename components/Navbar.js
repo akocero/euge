b6e61a20b3d65effb7fbc297f75ce7ac6b0ed998
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-08-28 21:43:11 +0800

    feat: added intro and base typog styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,46 +1,65 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const toggleNavbar = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('open')
-    burger.classList.toggle('open')
-}
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav");
+	nav.classList.toggle("open");
+	burger.classList.toggle("open");
+};
 
 const closeNavbar = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav');
-    nav.classList.remove('open')
-    burger.classList.remove('open')
-}
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav");
+	nav.classList.remove("open");
+	burger.classList.remove("open");
+};
 
 const Navbar = () => {
-    return ( 
-        <nav>
-            <div className="brand">
-                euge
-            </div>
-            <ul className="nav">
-                <li className="nav__item">
-                    <Link href="/" ><a className="nav__link" onClick={() => closeNavbar()}>About</a></Link>
-                </li>
-                <li className="nav__item">
-                    <Link href="/projects" ><a className="nav__link" onClick={() => closeNavbar()}>Projects</a></Link>
-                </li>
-                <li className="nav__item">
-                    <a href="" className="nav__link">Experiences</a>
-                </li>
-                <li className="nav__item">
-                    <a href="" className="nav__link">Contact</a>
-                </li>
-            </ul>
-            <div className="burger" onClick={() => toggleNavbar()}>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </nav>
-     );
-}
- 
+	return (
+		<header className="navbar">
+			<div className="h1">eugene badato.</div>
+			<nav>
+				<ul className="nav">
+					<li className="nav__item">
+						<a href="" className="nav__link">
+							Experiences
+						</a>
+					</li>
+					<li className="nav__item">
+						<Link href="/">
+							<a
+								className="nav__link"
+								onClick={() => closeNavbar()}
+							>
+								Skills
+							</a>
+						</Link>
+					</li>
+
+					<li className="nav__item">
+						<a href="" className="nav__link">
+							Contact
+						</a>
+					</li>
+					<li className="nav__item">
+						<Link href="/projects">
+							<a
+								className="nav__link"
+								onClick={() => closeNavbar()}
+							>
+								Projects
+							</a>
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			<div className="burger" onClick={() => toggleNavbar()}>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</header>
+	);
+};
+
 export default Navbar;
