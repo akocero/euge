d422c1606e84110bdd7b1829af7c8d5880e5e196
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-10-02 22:10:16 +0800

    feat: added card intitial styles

--- pages/index.js
+++ pages/index.js
@@ -31,23 +31,84 @@ export default function Home() {
 					</button>
 				</div>
 			</div>
-			<div className="card">
-				<div className="card__actions"></div>
-				<div className="card__content">
-					<h3 className="card__title">PelikulaPH</h3>
-					<h4 className="card__subtitle">
-						IMDB + Mobilarian Alternative
-					</h4>
-					<p className="card__body">
-						Lorem Ipsum is simply dummy text of the printing and
-						typesetting industry. Lorem Ipsum has been the
-						industry's.
-					</p>
+			<div className="d-grid">
+				<div className="card">
+					<div className="card__actions">
+						<button className="btn btn__link">
+							<FiGithub />
+						</button>
+						<button className="btn btn__link">
+							<FiGitlab />
+						</button>
+					</div>
+					<div className="card__content">
+						<h3 className="card__title">PelikulaPH</h3>
+						<h4 className="card__subtitle">
+							IMDB + Mobilarian Alternative
+						</h4>
+						<p className="card__body">
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting industry. Lorem Ipsum has been the
+							industry's.
+						</p>
+					</div>
+					<div className="tags">
+						<span>$javascript</span>
+						<span>$vue</span>
+						<span>$firebase</span>
+					</div>
 				</div>
-				<div className="tags">
-					<span>$javascript</span>
-					<span>$vue</span>
-					<span>$firebase</span>
+				<div className="card">
+					<div className="card__actions">
+						<button className="btn btn__link">
+							<FiGithub />
+						</button>
+						<button className="btn btn__link">
+							<FiGitlab />
+						</button>
+					</div>
+					<div className="card__content">
+						<h3 className="card__title">PelikulaPH</h3>
+						<h4 className="card__subtitle">
+							IMDB + Mobilarian Alternative
+						</h4>
+						<p className="card__body">
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting industry. Lorem Ipsum has been the
+							industry's.
+						</p>
+					</div>
+					<div className="tags">
+						<span>$javascript</span>
+						<span>$vue</span>
+						<span>$firebase</span>
+					</div>
+				</div>
+				<div className="card">
+					<div className="card__actions">
+						<button className="btn btn__link">
+							<FiGithub />
+						</button>
+						<button className="btn btn__link">
+							<FiGitlab />
+						</button>
+					</div>
+					<div className="card__content">
+						<h3 className="card__title">PelikulaPH</h3>
+						<h4 className="card__subtitle">
+							IMDB + Mobilarian Alternative
+						</h4>
+						<p className="card__body">
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting industry. Lorem Ipsum has been the
+							industry's.
+						</p>
+					</div>
+					<div className="tags">
+						<span>$javascript</span>
+						<span>$vue</span>
+						<span>$firebase</span>
+					</div>
 				</div>
 			</div>
 		</div>
