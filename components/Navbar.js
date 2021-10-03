7ec7463026fab77765235c6b0ddbe25b6f354962
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-10-03 17:12:23 +0800

    feat: added html template

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,63 +1,33 @@
 import Link from "next/link";
 
-const toggleNavbar = () => {
-	const burger = document.querySelector(".burger");
-	const nav = document.querySelector(".nav");
-	nav.classList.toggle("open");
-	burger.classList.toggle("open");
-};
-
-const closeNavbar = () => {
-	const burger = document.querySelector(".burger");
-	const nav = document.querySelector(".nav");
-	nav.classList.remove("open");
-	burger.classList.remove("open");
-};
-
 const Navbar = () => {
 	return (
 		<header className="navbar">
-			<div className="h1">eugene badato.</div>
-			<nav>
-				<ul className="nav">
-					<li className="nav__item">
-						<a href="" className="nav__link">
-							Experiences
+			<div className="name">eugene badato</div>
+			<nav className="navbar__nav">
+				<ul className="navbar__nav-list">
+					<li className="navbar__item">
+						<a href="" className="navbar__link">
+							projects
 						</a>
 					</li>
-					<li className="nav__item">
-						<Link href="/">
-							<a
-								className="nav__link"
-								onClick={() => closeNavbar()}
-							>
-								Skills
-							</a>
-						</Link>
-					</li>
-
-					<li className="nav__item">
-						<a href="" className="nav__link">
-							Contact
+					<li className="navbar__item">
+						<a href="" className="navbar__link">
+							experieces
 						</a>
 					</li>
-					<li className="nav__item">
-						<Link href="/projects">
-							<a
-								className="nav__link"
-								onClick={() => closeNavbar()}
-							>
-								Projects
-							</a>
-						</Link>
+					<li className="navbar__item">
+						<a href="" className="navbar__link">
+							about
+						</a>
+					</li>
+					<li className="navbar__item">
+						<a href="" className="navbar__link">
+							contact
+						</a>
 					</li>
 				</ul>
 			</nav>
-			<div className="burger" onClick={() => toggleNavbar()}>
-				<div></div>
-				<div></div>
-				<div></div>
-			</div>
 		</header>
 	);
 };
--- pages/index.js
+++ pages/index.js
@@ -13,103 +13,236 @@ import {
 export default function Home() {
 	return (
 		<div className="wrapper">
-			<div className="d-flex">
-				<button className="btn btn__primary">Project</button>
-				<div className="d-flex">
-					<button className="btn btn__link">
-						<FiGithub />
-					</button>
-					<button className="btn btn__link">
-						<FiGitlab />
-					</button>
-					<button className="btn btn__link">
-						<FiLinkedin />
-					</button>
-					<button className="btn btn__link">
-						<FiTwitter />
-					</button>
-				</div>
-			</div>
-			<div className="d-grid">
-				<div className="card">
-					<div className="card__actions">
+			<section className="intro">
+				<p>Hello, It’s Me Eugene.</p>
+				<h1>Let my portfolio describe me.</h1>
+
+				<div className="intro__actions d-flex">
+					<button className="btn btn__primary">project</button>
+
+					<div classNam="intro__links ">
 						<button className="btn btn__link">
 							<FiGithub />
 						</button>
 						<button className="btn btn__link">
-							<FiExternalLink />
+							<FiGitlab />
 						</button>
-					</div>
-					<div className="card__content">
-						<h3 className="card__title">PelikulaPH</h3>
-						<h4 className="card__subtitle">
-							IMDB + Mobilarian Alternative
-						</h4>
-						<p className="card__body">
-							Lorem Ipsum is simply dummy text of the printing and
-							typesetting industry. Lorem Ipsum has been the
-							industry's.
-						</p>
-					</div>
-					<div className="tags">
-						<span>$javascript</span>
-						<span>$vue</span>
-						<span>$firebase</span>
+						<button className="btn btn__link">
+							<FiLinkedin />
+						</button>
+						<button className="btn btn__link">
+							<FiTwitter />
+						</button>
 					</div>
 				</div>
-				<div className="card">
-					<div className="card__actions">
-						<button className="btn btn__link">
-							<FiGithub />
-						</button>
-						<button className="btn btn__link">
-							<FiExternalLink />
-						</button>
+			</section>
+
+			<section className="featured">
+				<div className="section__heading">
+					<h2>feature project</h2>
+					<label htmlFor="">what i'm doing?</label>
+				</div>
+
+				<article className="featured__item">
+					<div>
+						<div className="featured__img-container">
+							<img src="" alt="" />
+						</div>
+						<div className="featured__content">
+							<h3 className="featured__title">Pelikula PH</h3>
+							<label htmlFor="featured__subtitle">
+								IMDB + Mobilarian Alternative
+							</label>
+							<p className="feature_body">
+								Lorem Ipsum is simply dummy text of the printing
+								and typesetting industry. Lorem Ipsum has been
+								the industry's.
+							</p>
+							<div className="tags">
+								<span className="tag">$javascript</span>
+								<span className="tag">$vue</span>
+								<span className="tag">$firebase</span>
+							</div>
+						</div>
 					</div>
-					<div className="card__content">
-						<h3 className="card__title">PelikulaPH</h3>
-						<h4 className="card__subtitle">
-							IMDB + Mobilarian Alternative
-						</h4>
-						<p className="card__body">
-							Lorem Ipsum is simply dummy text of the printing and
-							typesetting industry. Lorem Ipsum has been the
-							industry's.
-						</p>
+				</article>
+
+				<article className="featured__item">
+					<div>
+						<div className="featured__img-container">
+							<img src="" alt="" />
+						</div>
+						<div className="featured__content">
+							<h3 className="featured__title">Pelikula PH</h3>
+							<label htmlFor="featured__subtitle">
+								IMDB + Mobilarian Alternative
+							</label>
+							<p className="feature_body">
+								Lorem Ipsum is simply dummy text of the printing
+								and typesetting industry. Lorem Ipsum has been
+								the industry's.
+							</p>
+							<div className="tags">
+								<span className="tag">$javascript</span>
+								<span className="tag">$vue</span>
+								<span className="tag">$firebase</span>
+							</div>
+						</div>
 					</div>
-					<div className="tags">
-						<span>$javascript</span>
-						<span>$vue</span>
-						<span>$firebase</span>
+				</article>
+			</section>
+			<section className="project">
+				<div className="section__heading">
+					<h2>noteworthy projects</h2>
+					<label htmlFor="">what i have done?</label>
+				</div>
+
+				<div className="card__list">
+					<div className="card">
+						<div className="card__actions">
+							<button className="btn btn__link">
+								<FiGithub />
+							</button>
+							<button className="btn btn__link">
+								<FiExternalLink />
+							</button>
+						</div>
+						<div className="card__content">
+							<h4 className="card__title">Pelikula PH</h4>
+							<h5 htmlFor="" className="card__subtitle">
+								IMDB + Mobilarian Alternative
+							</h5>
+
+							<div className="card__body">
+								Lorem Ipsum is simply dummy text of the printing
+								and typesetting industry. Lorem Ipsum has been
+								the industry's.
+							</div>
+						</div>
+
+						<div className="tags">
+							<span className="tag">$javascript</span>
+							<span className="tag">$vue</span>
+							<span className="tag">$firebase</span>
+						</div>
+					</div>
+					<div className="card">
+						<div className="card__actions">
+							<button className="btn btn__link">
+								<FiGithub />
+							</button>
+							<button className="btn btn__link">
+								<FiExternalLink />
+							</button>
+						</div>
+						<div className="card__content">
+							<h4 className="card__title">Pelikula PH</h4>
+							<h5 htmlFor="" className="card__subtitle">
+								IMDB + Mobilarian Alternative
+							</h5>
+
+							<div className="card__body">
+								Lorem Ipsum is simply dummy text of the printing
+								and typesetting industry. Lorem Ipsum has been
+								the industry's.
+							</div>
+						</div>
+
+						<div className="tags">
+							<span className="tag">$javascript</span>
+							<span className="tag">$vue</span>
+							<span className="tag">$firebase</span>
+						</div>
+					</div>
+					<div className="card">
+						<div className="card__actions">
+							<button className="btn btn__link">
+								<FiGithub />
+							</button>
+							<button className="btn btn__link">
+								<FiExternalLink />
+							</button>
+						</div>
+						<div className="card__content">
+							<h4 className="card__title">Pelikula PH</h4>
+							<h5 htmlFor="" className="card__subtitle">
+								IMDB + Mobilarian Alternative
+							</h5>
+
+							<div className="card__body">
+								Lorem Ipsum is simply dummy text of the printing
+								and typesetting industry. Lorem Ipsum has been
+								the industry's.
+							</div>
+						</div>
+
+						<div className="tags">
+							<span className="tag">$javascript</span>
+							<span className="tag">$vue</span>
+							<span className="tag">$firebase</span>
+						</div>
 					</div>
 				</div>
-				<div className="card">
-					<div className="card__actions">
-						<button className="btn btn__link">
-							<FiGithub />
-						</button>
-						<button className="btn btn__link">
-							<FiExternalLink />
-						</button>
-					</div>
-					<div className="card__content">
-						<h3 className="card__title">PelikulaPH</h3>
-						<h4 className="card__subtitle">
-							IMDB + Mobilarian Alternative
+			</section>
+			<section className="experience">
+				<div className="section__heading">
+					<h2>experiences</h2>
+					<label htmlFor="">what i have done?</label>
+				</div>
+
+				<div className="experiece__list">
+					<div className="experience__item">
+						<h4 className="experience__title">
+							Mustard Seed Systems Corp.
 						</h4>
-						<p className="card__body">
+						<h5 className="experience__subtitle"></h5>
+						<p className="experience__body">
 							Lorem Ipsum is simply dummy text of the printing and
-							typesetting industry. Lorem Ipsum has been the
-							industry's.
+							typesetting. (1 Year)
 						</p>
 					</div>
-					<div className="tags">
-						<span>$javascript</span>
-						<span>$vue</span>
-						<span>$firebase</span>
+					<div className="experience__item">
+						<h4 className="experience__title">
+							Mustard Seed Systems Corp.
+						</h4>
+						<h5 className="experience__subtitle"></h5>
+						<p className="experience__body">
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting. (1 Year)
+						</p>
+					</div>
+					<div className="experience__item">
+						<h4 className="experience__title">
+							Mustard Seed Systems Corp.
+						</h4>
+						<h5 className="experience__subtitle"></h5>
+						<p className="experience__body">
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting. (1 Year)
+						</p>
+					</div>
+					<div className="experience__item">
+						<h4 className="experience__title">
+							Mustard Seed Systems Corp.
+						</h4>
+						<h5 className="experience__subtitle"></h5>
+						<p className="experience__body">
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting. (1 Year)
+						</p>
 					</div>
 				</div>
-			</div>
+			</section>
+			<section className="about">
+				<div className="section__heading">
+					<h2>about</h2>
+					<label htmlFor="">what i love?</label>
+				</div>
+				<div>
+					<div></div>
+					<div></div>
+				</div>
+			</section>
 		</div>
 	);
 }
