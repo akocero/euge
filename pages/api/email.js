fc445198200f134740458a80564abcfa061b12d0
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-04-04 14:30:34 +0800

    added host

--- pages/api/email.js
+++ pages/api/email.js
@@ -7,6 +7,7 @@ export default function handler(req, res) {
 	res.status(200).json({ status: "Ok" });
 
 	let transporter = nodemailer.createTransport({
+		host: "localhost",
 		service: "gmail",
 		port: 465,
 		secure: true,
