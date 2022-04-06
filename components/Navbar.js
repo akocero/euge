a1997991756c15ea45b0f01b2358919d94e34bdb
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-04-06 18:56:50 +0800

    added sticky header

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,6 +6,10 @@ import { useState, useEffect } from "react";
 const Navbar = () => {
 	const [isDark, setIsDark] = useState("");
 	let darkMode = "";
+
+	const [lastScroll, setLastScroll] = useState(0);
+	const [scrollState, setScrollState] = useState("");
+
 	useEffect(() => {
 		// setIsDark(localStorage.getItem("darkmode"));
 		darkMode = localStorage.getItem("darkmode");
@@ -15,6 +19,30 @@ const Navbar = () => {
 			disableDarkMode();
 		}
 	}, []);
+
+	useEffect(() => {
+		const onScroll = (e) => {
+			const currentScroll = window.pageYOffset;
+
+			if (currentScroll > lastScroll) {
+				setScrollState("scroll-down");
+			}
+
+			if (currentScroll < lastScroll) {
+				setScrollState("scroll-up");
+			}
+
+			if (currentScroll <= 0) {
+				setScrollState("");
+			}
+
+			setLastScroll(currentScroll);
+		};
+
+		window.addEventListener("scroll", onScroll);
+
+		return () => window.removeEventListener("scroll", onScroll);
+	}, [scrollState, lastScroll]);
 
 	const enableDarkMode = () => {
 		document.body.classList.add("dark");
@@ -49,49 +77,51 @@ const Navbar = () => {
 	};
 
 	return (
-		<header className="navbar">
-			<div className="navbar__name">
-				<Link href="/">
-					<a>@eugenebadato.</a>
-				</Link>
+		<header className={`navbar ${scrollState}`}>
+			<div className="navbar__container">
+				<div className="navbar__name">
+					<Link href="/">
+						<a>@eugenebadato.</a>
+					</Link>
+				</div>
+				<nav className="navbar__nav">
+					<ul className="navbar__nav-list">
+						<li className="navbar__item">
+							<Link href="#home">
+								<a className="navbar__link">home</a>
+							</Link>
+						</li>
+						<li className="navbar__item">
+							<Link href="#projects">
+								<a className="navbar__link">projects</a>
+							</Link>
+						</li>
+						<li className="navbar__item">
+							<Link href="#experiences">
+								<a className="navbar__link">experiences</a>
+							</Link>
+						</li>
+						<li className="navbar__item">
+							<Link href="#about">
+								<a className="navbar__link">about</a>
+							</Link>
+						</li>
+						<li className="navbar__item">
+							<Link href="#contact">
+								<a className="navbar__link">contact</a>
+							</Link>
+						</li>
+					</ul>
+				</nav>
+				<button
+					className="navbar__toggle"
+					onClick={() => {
+						toggleDarkMode();
+					}}
+				>
+					{isDark == "dark" ? <RiSunLine /> : <RiMoonClearLine />}
+				</button>
 			</div>
-			<nav className="navbar__nav">
-				<ul className="navbar__nav-list">
-					<li className="navbar__item">
-						<Link href="#home">
-							<a className="navbar__link">home</a>
-						</Link>
-					</li>
-					<li className="navbar__item">
-						<Link href="#projects">
-							<a className="navbar__link">projects</a>
-						</Link>
-					</li>
-					<li className="navbar__item">
-						<Link href="#experiences">
-							<a className="navbar__link">experiences</a>
-						</Link>
-					</li>
-					<li className="navbar__item">
-						<Link href="#about">
-							<a className="navbar__link">about</a>
-						</Link>
-					</li>
-					<li className="navbar__item">
-						<Link href="#contact">
-							<a className="navbar__link">contact</a>
-						</Link>
-					</li>
-				</ul>
-			</nav>
-			<button
-				className="navbar__toggle"
-				onClick={() => {
-					toggleDarkMode();
-				}}
-			>
-				{isDark == "dark" ? <RiSunLine /> : <RiMoonClearLine />}
-			</button>
 		</header>
 	);
 };
