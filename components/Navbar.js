4f18ce6354f7e9d9451a6d870377966590a23d9a
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-04-06 15:22:39 +0800

    feat: added dark and light mode

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,53 @@
 import Link from "next/link";
+import { FiSun, FiMoon } from "react-icons/fi";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+	const [isDark, setIsDark] = useState("");
+	let darkMode = "";
+	useEffect(() => {
+		// setIsDark(localStorage.getItem("darkmode"));
+		darkMode = localStorage.getItem("darkmode");
+		if (darkMode == "dark") {
+			enableDarkMode();
+			console.log("umabot");
+		} else {
+			disableDarkMode();
+		}
+	}, []);
+
+	const enableDarkMode = () => {
+		document.body.classList.add("dark");
+		const body = document.querySelector(".dark");
+		body.setAttribute("class", "dark");
+
+		localStorage.setItem("darkmode", "dark");
+		setIsDark("dark");
+
+		console.log("local: dark");
+	};
+
+	const disableDarkMode = () => {
+		document.body.classList.add("light");
+		const body = document.querySelector(".light");
+		body.setAttribute("class", "light");
+
+		localStorage.setItem("darkmode", "light");
+		setIsDark("light");
+
+		console.log("local: light");
+	};
+
+	const toggleDarkMode = () => {
+		darkMode = localStorage.getItem("darkmode");
+
+		if (darkMode === "dark") {
+			disableDarkMode();
+		} else {
+			enableDarkMode();
+		}
+	};
+
 	return (
 		<header className="navbar">
 			<div className="navbar__name">
@@ -32,11 +79,19 @@ const Navbar = () => {
 					</li>
 					<li className="navbar__item">
 						<Link href="#contact">
-							<a className="navbar__link">conatct</a>
+							<a className="navbar__link">contact</a>
 						</Link>
 					</li>
 				</ul>
 			</nav>
+			<button
+				className="navbar__toggle"
+				onClick={() => {
+					toggleDarkMode();
+				}}
+			>
+				{isDark == "dark" ? <FiSun /> : <FiMoon />}
+			</button>
 		</header>
 	);
 };
