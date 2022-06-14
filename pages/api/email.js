5e3cc6b01c58d9d06510a5808138baf57f7866b9
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-06-14 17:23:17 +0800

    feat: found the bug hehe

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,14 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 export default function handler(req, res) {
 	const body = JSON.parse(req.body);
 	console.log(body);
-	res.status(200).json({ status: "Ok" });
 
 	let transporter = nodemailer.createTransport({
-		host: "https://eugenebadato.vercel.app/",
-		service: "gmail",
+		host: 'https://eugenebadato.vercel.app/',
+		service: 'gmail',
 		port: 465,
 		secureConnection: true,
 		auth: {
@@ -25,18 +24,21 @@ export default function handler(req, res) {
 	let mailOptions = {
 		from: body.email,
 		to: process.env.MAIL_EMAIL,
-		subject: "Email From My Portfolio",
+		subject: 'Email From My Portfolio',
 		text: message,
-		html: message.replace(/\r\n/g, "<br>"),
+		html: message.replace(/\r\n/g, '<br>'),
 	};
 
 	transporter.sendMail(mailOptions, (err, data) => {
 		if (err) {
-			return console.log("Error occurs", err);
-		}else{
-			console.log('email sent')
+			res.status(400).json({ status: 'Bad' });
+			console.log('Error occurs', err);
+			return;
+		} else {
+			console.log('email sent');
+			res.status(200).json({ status: 'Ok' });
 			return;
 		}
-		return console.log("Email sent!!!");
+		return console.log('Email sent!!!');
 	});
 }
